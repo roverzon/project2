baf7c1b174dbbd50c83a9a2b074b0a1194faea3e
Roverzon Li <roverzon@gmail.com>
2021-04-04 13:07:24 +0800

    fix api bugs

--- balance_sheets/urls.py
+++ balance_sheets/urls.py
@@ -3,9 +3,9 @@ from balance_sheets import views
 
 
 urlpatterns = [
-    url(r'^api/balancesheets$', views.balancesheet_list),
+    url(r'^api/balancesheets$', views.balance_sheet_list),
     url(r'^api/balancesheets/init/all$', views.balance_sheet_init_async),
-    url(r'^api/balancesheets/(?P<symbol>[\w\-]+)$', views.symbol_balancesheet_list),
-    url(r'^api/v2/balancesheets/annual/(?P<symbol>[\w\-]+)$', views.alpha_vantage_balancesheet_annual),
-    url(r'^api/v2/balancesheets/quarterly/(?P<symbol>[\w\-]+)$', views.alpha_vantage_balancesheet_quarterly),
+    url(r'^api/balancesheets/(?P<symbol>[\w\-]+)$', views.symbol_balance_sheet_list),
+    url(r'^api/v2/balancesheets/annual/(?P<symbol>[\w\-]+)$', views.alpha_vantage_balance_sheet_annual),
+    url(r'^api/v2/balancesheets/quarterly/(?P<symbol>[\w\-]+)$', views.alpha_vantage_balance_sheet_quarterly),
 ]
